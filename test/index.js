4eb9eb286c222ca445e05228208a1e93c9f5e838
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-01-03 22:17:41 +0100

    test(cache): test that caching function can be disable

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
 'use strict'
 
 import superapiCache from '../lib/index.js'
-import test from 'tape'
+import test from 'blue-tape'
+import { spy } from 'sinon'
 
 test('middleware configuration', t => {
   t.throws(() => {
@@ -24,4 +25,54 @@ test('middleware configuration', t => {
     })
   }, 'should not throw if valid store provided')
   t.end()
+})
+
+
+test('should call cache function by default', t => {
+  const cacheFn = spy()
+  const handler = superapiCache({
+    store: {
+      cache: cacheFn
+    }
+  })
+
+  const next = () => {
+    return Promise.resolve()
+  }
+  const service = {}
+
+  return new Promise((resolve) => {
+    handler({}, next, service).then(() => {
+      t.ok(cacheFn.called, 'cache function should have been called')
+      t.ok(cacheFn.calledOnce, 'cache function should have been called once')
+
+      resolve()
+    })
+  })
+})
+
+test('disable cache', t => {
+  const cacheFn = spy(function () {})
+  const handler = superapiCache({
+    store: {
+      cache: cacheFn
+    }
+  })
+
+  const next = () => {
+    return Promise.resolve()
+  }
+  const service = {
+    use: {
+      cache: false
+    }
+  }
+
+  return new Promise((resolve) => {
+    handler({}, next, service).then(() => {
+      t.ok(cacheFn.notCalled, 'cache function should not have been called')
+
+      resolve()
+    })
+  })
 })
