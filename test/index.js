a80adf38387fd3315bd9745b1eb700a942bc3ff8
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-01-03 21:46:40 +0100

    test(cache): add test about configuration

--- test/index.js
+++ test/index.js
@@ -3,9 +3,25 @@
 import superapiCache from '../lib/index.js'
 import test from 'tape'
 
-test('awesome:test', t => {
-  const message = 'everything is awesome'
-  t.equals(superapiCache('awesome'), message, message)
+test('middleware configuration', t => {
+  t.throws(() => {
+    superapiCache()
+  }, /Cache middleware need to be provided a store/, 'should throw if no store property found')
+
+  t.throws(() => {
+    superapiCache({
+      store: {
+        foo: 'bar'
+      }
+    })
+  }, /Store does not have a cache function/, 'should throw if invalid store provided')
+
+  t.doesNotThrow(() => {
+    superapiCache({
+      store: {
+        cache: function () {}
+      }
+    })
+  }, 'should not throw if valid store provided')
   t.end()
 })
-
