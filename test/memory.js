2444a12a27ebd93ccdf34607a0d2b6fbdb6a0bb9
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-01-04 11:25:42 +0100

    test(memory): convert test to promise

--- test/memory.js
+++ test/memory.js
@@ -9,9 +9,13 @@ test('MemoryStore getItem', t => {
 
   store._store.foo = expected
 
-  t.equals(store.getItem('foo'), expected, 'store should return the previously recorded value')
+  return new Promise((resolve) => {
+    store.getItem('foo').then((value) => {
+      t.equals(value, expected, 'store should return the previously recorded value')
 
-  t.end()
+      resolve()
+    })
+  })
 })
 
 test('MemoryStore setItem', t => {
@@ -19,20 +23,31 @@ test('MemoryStore setItem', t => {
 
   const expected = 'bar'
 
-  store.setItem('foo', expected)
+  return new Promise((resolve) => {
+    store.setItem('foo', expected).then(() => {
+      store.getItem('foo').then((value) => {
+        t.equals(value, expected, 'store should return the previously recorded value')
 
-  t.equals(store.getItem('foo'), expected, 'store should return the previously recorded value')
-
-  t.end()
+        resolve()
+      })
+    })
+  })
 })
 
 test('MemoryStore clear', t => {
   const store = new MemoryStore()
 
-  store.setItem('foo', 1234)
-  store.clear()
+  store._store.foo = 'something'
 
-  t.equals(store.getItem('foo'), null, 'store should be emtpy');
+  return new Promise((resolve) => {
+    store.clear()
+      .then(() => {
+        return store.getItem('foo')
+      })
+      .then((value) => {
+        t.equals(value, null, 'store should be emtpy');
 
-  t.end()
+        resolve()
+      })
+  })
 })
