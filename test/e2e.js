e9c60d1acb948f7278e6dafc9822e5d091d2d14e
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-01-08 22:55:05 +0100

    test(cache): add test to verify network response is cached

--- test/e2e.js
+++ test/e2e.js
@@ -9,6 +9,7 @@ const log = debug('cache')
 import superapiCache from '../lib/index.js'
 import MemoryStore from '../lib/memory.js'
 import hydrate from '../lib/hydrate.js'
+import serialize from '../lib/serialize'
 
 import req from './helpers/req'
 import response from './helpers/response';
@@ -54,7 +55,7 @@ test('miss from cache', t => {
     }
 
     req.url = '/api/foo'
-    req.set('response', hydrate(JSON.stringify(fixtures)))
+    req.xhr = hydrate(JSON.stringify(fixtures))
 
     const fetchNetwork = spy(req, 'response')
     const next = () => {
@@ -67,15 +68,58 @@ test('miss from cache', t => {
         t.equal(fixtures.body.status, res.status, 'should retrieve the same status from cache')
         t.equal(fixtures.body.responseText, res.responseText, 'should retrieve the same responseText from cache')
 
+        req.response.restore()
         resolve()
       }).catch((err) => {
         t.fail('should not throw error')
+
+        req.response.restore()
         resolve()
       })
   })
 })
 
-test.skip('fetch network', t => {
-  t.ok(false, 'should add to cache on 2xx')
-  t.end()
+test('fetch network', t => {
+  return new Promise((resolve) => {
+    const fixtures = require('./fixtures/hello');
+    const store = new MemoryStore()
+    const _serialize = spy(serialize)
+
+    const options = {
+      store: store,
+      serialize: _serialize
+    }
+
+    req.url = '/api/foo'
+    req.xhr = hydrate(JSON.stringify(fixtures))
+
+    const fetchNetwork = spy(req, 'response')
+    const next = () => {
+      return Promise.resolve(req.response())
+    }
+
+    return superapiCache(options)(req, next, {})
+      .then((res) => {
+        t.ok(fetchNetwork.called, 'should fetch response from network')
+
+        store.getItem(req.url)
+          .then((value) => {
+            t.ok(value, 'it should have copy the response in the cache')
+            t.ok(_serialize.called, 'it should have serialized the response in the cache')
+            t.equal(value.body.status, res.status, 'it should have the same status from network')
+            t.equal(value.body.responseText, res.responseText, 'it should have the same responseText from network')
+            t.equal(value.headers['content-type'], res.getResponseHeader('content-type'), 'it should have the same responseText from network')
+
+            req.response.restore()
+            resolve()
+          })
+
+      }).catch((err) => {
+        console.log(err);
+        t.fail('should not throw error')
+
+        req.response.restore()
+        resolve()
+      })
+  })
 })
