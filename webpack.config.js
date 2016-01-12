326d96ad268cd7966c5f2c4e8b02e91629aaf165
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-01-12 07:18:08 +0100

    chore(bundle): use webpack to compile library bundle

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+var path = require('path');
+
+var libraryName = 'superapi-cache';
+
+var plugins = [], outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = libraryName + '.min.js';
+} else {
+  outputFile = libraryName + '.js';
+}
+
+var config = {
+  entry: __dirname + '/lib/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./lib'),
+    extensions: ['', '.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;
