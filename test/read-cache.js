98120e43330169c92fec0a651e38e33bc9f37fb4
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-03-06 16:43:10 +0100

    test(read-cache): add log
    Also change style to not use parentheses for one arg function

--- test/read-cache.js
+++ test/read-cache.js
@@ -3,6 +3,9 @@
 import readCache from '../lib/read-cache'
 import test from 'blue-tape'
 import Request from './helpers/req'
+import debug from 'debug'
+
+const log = debug('readCache')
 
 test('readCache closure', t => {
   const closure = readCache({})
@@ -34,14 +37,14 @@ test('readCache success callback', t => {
 
   const req = new Request()
 
-  return new Promise((resolve) => {
-      .then((res) => {
-    readCache(req)(value)
+  return new Promise(resolve => {
+    readCache(req, log)(value)
+      .then(res => {
         t.equals(res.body, value.data.body.responseText, 'response should be defined')
 
         resolve()
       })
-      .catch((err) => {
+      .catch(err => {
         // add test in case of error
         t.error(err, 'hydration should not throw')
 
@@ -51,16 +54,16 @@ test('readCache success callback', t => {
 })
 
 test('readCache error callback', t => {
-  return new Promise((resolve) => {
-    readCache(req)(false)
-      .then((res) => {
   const req = new Request()
 
+  return new Promise(resolve => {
+    readCache(req, log)(false)
+      .then(res => {
         t.error('response should not be defined on cache miss')
 
         resolve()
       })
-      .catch((err) => {
+      .catch(err => {
         // add test in case of error
         t.equals(err.reason, 'cache-miss', 'reading from cache should throw on cache miss')
 
