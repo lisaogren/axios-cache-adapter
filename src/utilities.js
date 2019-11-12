80ca834a620be98feb4db0092e2b8137fa3188e8
Raphael Beer <raphael.beer@protonmail.com>
2019-11-12 15:33:08 +0100

    Chore: mention lodash source for utilities functions; lint

--- src/utilities.js
+++ src/utilities.js
@@ -1,42 +1,45 @@
-/* eslint-disable */
-export function isObject(value) {
+// https://github.com/lodash/lodash/blob/master/isObject.js
+export function isObject (value) {
   const type = typeof value
-  return value != null && (type == 'object' || type == 'function')
+  return value != null && (type === 'object' || type === 'function')
 }
 
-export function getTag(value) {
-  if (value == null) {
+// https://github.com/lodash/lodash/blob/master/.internal/getTag.js
+export function getTag (value) {
+  if (value === null) {
     return value === undefined ? '[object Undefined]' : '[object Null]'
   }
   return toString.call(value)
 }
 
-export function isFunction(value) {
+// https://github.com/lodash/lodash/blob/master/isFunction.js
+export function isFunction (value) {
   if (!isObject(value)) {
     return false
   }
 
   const tag = getTag(value)
   return (
-    tag == '[object Function]' ||
-    tag == '[object AsyncFunction]' ||
-    tag == '[object GeneratorFunction]' ||
-    tag == '[object Proxy]'
+    tag === '[object Function]' ||
+    tag === '[object AsyncFunction]' ||
+    tag === '[object GeneratorFunction]' ||
+    tag === '[object Proxy]'
   )
 }
 
-export function isString(value) {
+// https://github.com/lodash/lodash/blob/master/isString.js
+export function isString (value) {
   const type = typeof value
   return (
-    type == "string" ||
-    (type == "object" &&
+    type === 'string' ||
+    (type === 'object' &&
       value != null &&
       !Array.isArray(value) &&
-      getTag(value) == "[object String]")
+      getTag(value) === '[object String]')
   )
 }
 
-export function mapObject(value, fn) {
+export function mapObject (value, fn) {
   if (!isObject(value)) {
     return []
   }
